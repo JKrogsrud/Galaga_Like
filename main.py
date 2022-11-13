e94dec6e013ea95f41ea983be78237d8871734f2
Jared Krogsrud <JKrogsrud@gmail.com>
2022-11-12 20:34:35 -0500

    2nd level boss

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ python -m arcade.examples.starting_template
 """
 import arcade
 from bullet import Bullet
+from weapon import Weapon
 from player import Player
 from enemy import Enemy, create_random_level_one_bug, create_random_level_two_bug, create_random_level_three_bug, create_L4_bug
 from enemy import FiringPattern, create_swarmer, create_singleton
@@ -140,9 +141,9 @@ class MyGame(arcade.View):
 
         self.level = 2
 
-        self.wave = 7
+        self.wave = 0
 
-        self.time = 30
+        self.time = 0
 
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
@@ -275,7 +276,7 @@ class MyGame(arcade.View):
                 )
 
             # Bullet is off the below screen
-            if bullet.bottom < 0:
+            if bullet.top < 0:
                 bullet.remove_from_sprite_lists()
 
 
@@ -675,7 +676,6 @@ class MyGame(arcade.View):
                     enemy_row.append(enemy)
 
                 self.enemy_list.append(enemy_row)
-            pass
 
             if self.time > 25 and self.wave == 3:
                 self.wave += 1
@@ -713,175 +713,200 @@ class MyGame(arcade.View):
 
                 self.enemy_list.append(enemy_row)
 
-        if self.time > 27 and self.wave == 4:
-            self.wave += 1
+            if self.time > 27 and self.wave == 4:
+                self.wave += 1
 
-            enemy_row = arcade.SpriteList()
+                enemy_row = arcade.SpriteList()
 
-            trajectory_left = [(160, SCREEN_HEIGHT + 10),
-                               (160, 3 * SCREEN_HEIGHT / 4),
-                               (120, 3 * SCREEN_HEIGHT / 4 - 40),
-                               (160, 3 * SCREEN_HEIGHT / 4 - 80),
-                               (200, 3 * SCREEN_HEIGHT / 4 - 120)]
-            destination_left = (40, -100)
-            firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+                trajectory_left = [(160, SCREEN_HEIGHT + 10),
+                                   (160, 3 * SCREEN_HEIGHT / 4),
+                                   (120, 3 * SCREEN_HEIGHT / 4 - 40),
+                                   (160, 3 * SCREEN_HEIGHT / 4 - 80),
+                                   (200, 3 * SCREEN_HEIGHT / 4 - 120)]
+                destination_left = (40, -100)
+                firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
 
-            enemy_1 = create_singleton(trajectory_left, destination_left, firing_pattern_1)
-            enemy_1.center_x, enemy_1.center_y = 150, SCREEN_HEIGHT + 20
-            enemy_1.angle = 180
+                enemy_1 = create_singleton(trajectory_left, destination_left, firing_pattern_1)
+                enemy_1.center_x, enemy_1.center_y = 150, SCREEN_HEIGHT + 20
+                enemy_1.angle = 180
 
-            trajectory_right = [(SCREEN_WIDTH - 160, SCREEN_HEIGHT + 10),
-                                (SCREEN_WIDTH - 160, 3 * SCREEN_HEIGHT / 4),
-                                (SCREEN_WIDTH - 120, 3 * SCREEN_HEIGHT / 4 - 40),
-                                (SCREEN_WIDTH - 160, 3 * SCREEN_HEIGHT / 4 - 80),
-                                (SCREEN_WIDTH - 200, 3 * SCREEN_HEIGHT / 4 - 120)]
+                trajectory_right = [(SCREEN_WIDTH - 160, SCREEN_HEIGHT + 10),
+                                    (SCREEN_WIDTH - 160, 3 * SCREEN_HEIGHT / 4),
+                                    (SCREEN_WIDTH - 120, 3 * SCREEN_HEIGHT / 4 - 40),
+                                    (SCREEN_WIDTH - 160, 3 * SCREEN_HEIGHT / 4 - 80),
+                                    (SCREEN_WIDTH - 200, 3 * SCREEN_HEIGHT / 4 - 120)]
 
-            destination_right = (SCREEN_WIDTH - 40, -100)
-            firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+                destination_right = (SCREEN_WIDTH - 40, -100)
+                firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
 
-            enemy_2 = create_singleton(trajectory_right, destination_right, firing_pattern_1)
-            enemy_2.center_x, enemy_2.center_y = SCREEN_WIDTH - 150, SCREEN_HEIGHT + 20
-            enemy_2.speed_to_formation = 1
-            enemy_2.angle = 180
+                enemy_2 = create_singleton(trajectory_right, destination_right, firing_pattern_1)
+                enemy_2.center_x, enemy_2.center_y = SCREEN_WIDTH - 150, SCREEN_HEIGHT + 20
+                enemy_2.speed_to_formation = 1
+                enemy_2.angle = 180
 
-            enemy_row.append(enemy_1)
-            enemy_row.append(enemy_2)
+                enemy_row.append(enemy_1)
+                enemy_row.append(enemy_2)
 
-            self.enemy_list.append(enemy_row)
+                self.enemy_list.append(enemy_row)
 
-        if self.time > 29 and self.wave == 5:
-            self.wave += 1
+            if self.time > 29 and self.wave == 5:
+                self.wave += 1
 
-            enemy_row = arcade.SpriteList()
+                enemy_row = arcade.SpriteList()
 
-            trajectory_left = [(170, SCREEN_HEIGHT + 10),
-                               (170, 3 * SCREEN_HEIGHT / 4),
-                               (130, 3 * SCREEN_HEIGHT / 4 - 40),
-                               (170, 3 * SCREEN_HEIGHT / 4 - 80),
-                               (210, 3 * SCREEN_HEIGHT / 4 - 120)]
-            destination_left = (40, -100)
-            firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+                trajectory_left = [(170, SCREEN_HEIGHT + 10),
+                                   (170, 3 * SCREEN_HEIGHT / 4),
+                                   (130, 3 * SCREEN_HEIGHT / 4 - 40),
+                                   (170, 3 * SCREEN_HEIGHT / 4 - 80),
+                                   (210, 3 * SCREEN_HEIGHT / 4 - 120)]
+                destination_left = (40, -100)
+                firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
 
-            enemy_1 = create_singleton(trajectory_left, destination_left, firing_pattern_1)
-            enemy_1.center_x, enemy_1.center_y = 150, SCREEN_HEIGHT + 20
-            enemy_1.angle = 180
+                enemy_1 = create_singleton(trajectory_left, destination_left, firing_pattern_1)
+                enemy_1.center_x, enemy_1.center_y = 150, SCREEN_HEIGHT + 20
+                enemy_1.angle = 180
 
-            trajectory_right = [(SCREEN_WIDTH - 170, SCREEN_HEIGHT + 10),
-                                (SCREEN_WIDTH - 170, 3 * SCREEN_HEIGHT / 4),
-                                (SCREEN_WIDTH - 130, 3 * SCREEN_HEIGHT / 4 - 40),
-                                (SCREEN_WIDTH - 170, 3 * SCREEN_HEIGHT / 4 - 80),
-                                (SCREEN_WIDTH - 210, 3 * SCREEN_HEIGHT / 4 - 120)]
+                trajectory_right = [(SCREEN_WIDTH - 170, SCREEN_HEIGHT + 10),
+                                    (SCREEN_WIDTH - 170, 3 * SCREEN_HEIGHT / 4),
+                                    (SCREEN_WIDTH - 130, 3 * SCREEN_HEIGHT / 4 - 40),
+                                    (SCREEN_WIDTH - 170, 3 * SCREEN_HEIGHT / 4 - 80),
+                                    (SCREEN_WIDTH - 210, 3 * SCREEN_HEIGHT / 4 - 120)]
 
-            destination_right = (SCREEN_WIDTH - 40, -100)
-            firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+                destination_right = (SCREEN_WIDTH - 40, -100)
+                firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
 
-            enemy_2 = create_singleton(trajectory_right, destination_right, firing_pattern_1)
-            enemy_2.center_x, enemy_2.center_y = SCREEN_WIDTH - 150, SCREEN_HEIGHT + 20
-            enemy_2.speed_to_formation = 1
-            enemy_2.angle = 180
+                enemy_2 = create_singleton(trajectory_right, destination_right, firing_pattern_1)
+                enemy_2.center_x, enemy_2.center_y = SCREEN_WIDTH - 150, SCREEN_HEIGHT + 20
+                enemy_2.speed_to_formation = 1
+                enemy_2.angle = 180
 
-            enemy_row.append(enemy_1)
-            enemy_row.append(enemy_2)
+                enemy_row.append(enemy_1)
+                enemy_row.append(enemy_2)
 
-            self.enemy_list.append(enemy_row)
+                self.enemy_list.append(enemy_row)
 
-        if self.time > 31 and self.wave == 6:
-            self.wave += 1
+            if self.time > 31 and self.wave == 6:
+                self.wave += 1
 
-            enemy_row = arcade.SpriteList()
+                enemy_row = arcade.SpriteList()
 
-            trajectory_left = [(180, SCREEN_HEIGHT + 10),
-                               (180, 3 * SCREEN_HEIGHT / 4),
-                               (140, 3 * SCREEN_HEIGHT / 4 - 40),
-                               (180, 3 * SCREEN_HEIGHT / 4 - 80),
-                               (220, 3 * SCREEN_HEIGHT / 4 - 120)]
-            destination_left = (40, -100)
-            firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+                trajectory_left = [(180, SCREEN_HEIGHT + 10),
+                                   (180, 3 * SCREEN_HEIGHT / 4),
+                                   (140, 3 * SCREEN_HEIGHT / 4 - 40),
+                                   (180, 3 * SCREEN_HEIGHT / 4 - 80),
+                                   (220, 3 * SCREEN_HEIGHT / 4 - 120)]
+                destination_left = (40, -100)
+                firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
 
-            enemy_1 = create_singleton(trajectory_left, destination_left, firing_pattern_1)
-            enemy_1.center_x, enemy_1.center_y = 150, SCREEN_HEIGHT + 20
-            enemy_1.angle = 180
+                enemy_1 = create_singleton(trajectory_left, destination_left, firing_pattern_1)
+                enemy_1.center_x, enemy_1.center_y = 150, SCREEN_HEIGHT + 20
+                enemy_1.angle = 180
 
-            trajectory_right = [(SCREEN_WIDTH - 180, SCREEN_HEIGHT + 10),
-                                (SCREEN_WIDTH - 180, 3 * SCREEN_HEIGHT / 4),
-                                (SCREEN_WIDTH - 140, 3 * SCREEN_HEIGHT / 4 - 40),
-                                (SCREEN_WIDTH - 180, 3 * SCREEN_HEIGHT / 4 - 80),
-                                (SCREEN_WIDTH - 220, 3 * SCREEN_HEIGHT / 4 - 120)]
+                trajectory_right = [(SCREEN_WIDTH - 180, SCREEN_HEIGHT + 10),
+                                    (SCREEN_WIDTH - 180, 3 * SCREEN_HEIGHT / 4),
+                                    (SCREEN_WIDTH - 140, 3 * SCREEN_HEIGHT / 4 - 40),
+                                    (SCREEN_WIDTH - 180, 3 * SCREEN_HEIGHT / 4 - 80),
+                                    (SCREEN_WIDTH - 220, 3 * SCREEN_HEIGHT / 4 - 120)]
 
-            destination_right = (SCREEN_WIDTH - 40, -100)
-            firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
+                destination_right = (SCREEN_WIDTH - 40, -100)
+                firing_pattern_1 = FiringPattern([1.2, 0.2, 0.2, 0.2, 0.2, 0.2])
 
-            enemy_2 = create_singleton(trajectory_right, destination_right, firing_pattern_1)
-            enemy_2.center_x, enemy_2.center_y = SCREEN_WIDTH - 150, SCREEN_HEIGHT + 20
-            enemy_2.speed_to_formation = 1
-            enemy_2.angle = 180
+                enemy_2 = create_singleton(trajectory_right, destination_right, firing_pattern_1)
+                enemy_2.center_x, enemy_2.center_y = SCREEN_WIDTH - 150, SCREEN_HEIGHT + 20
+                enemy_2.speed_to_formation = 1
+                enemy_2.angle = 180
 
-            enemy_row.append(enemy_1)
-            enemy_row.append(enemy_2)
+                enemy_row.append(enemy_1)
+                enemy_row.append(enemy_2)
 
-            self.enemy_list.append(enemy_row)
+                self.enemy_list.append(enemy_row)
 
-        if self.time > 33 and self.wave == 7:
-            self.wave += 1
+            if self.time > 33 and self.wave == 7:
+                self.wave += 1
 
-            enemy_row = arcade.SpriteList()
+                enemy_row = arcade.SpriteList()
 
-            # Prepare the various trajectories about to be used
+                # Prepare the various trajectories about to be used
 
-            trajectory_1 = circle_trajectory(75, (SCREEN_WIDTH-100, SCREEN_HEIGHT-100), math.pi/2, 50, 9*math.pi/2)
-            trajectory_2 = circle_trajectory(330, (400, 325), 2*math.pi/3, 60, math.pi)
-            trajectory_3 = circle_trajectory(50, (70, 275), math.pi/2, 100, 7*math.pi/2)
-            trajectory_4 = circle_trajectory(70, (SCREEN_WIDTH-150, SCREEN_HEIGHT-450), 3*math.pi/2, 50, 9* math.pi/2)
-            trajectory_4.reverse()
+                trajectory_1 = circle_trajectory(75, (SCREEN_WIDTH-100, SCREEN_HEIGHT-100), math.pi/2, 50, 9*math.pi/2)
+                trajectory_2 = circle_trajectory(330, (400, 325), 2*math.pi/3, 60, math.pi)
+                trajectory_3 = circle_trajectory(50, (70, 275), math.pi/2, 100, 7*math.pi/2)
+                trajectory_4 = circle_trajectory(70, (SCREEN_WIDTH-150, SCREEN_HEIGHT-450), 3*math.pi/2, 50, 9* math.pi/2)
+                trajectory_4.reverse()
 
-            trajectory_1.extend(trajectory_2)
-            trajectory_1.extend(trajectory_3)
-            trajectory_1.extend(trajectory_4)
+                trajectory_1.extend(trajectory_2)
+                trajectory_1.extend(trajectory_3)
+                trajectory_1.extend(trajectory_4)
 
-            # All of the following will be used as 4 separate branches for the trajectory above
-            trajectory_a = circle_trajectory(150, (175, 0), math.pi / 2, 60, 9 * math.pi / 2)
-            trajectory_b = circle_trajectory(150, (325, 0), math.pi / 2, 60, 9 * math.pi / 2)
-            trajectory_b.reverse()
+                # All of the following will be used as 4 separate branches for the trajectory above
+                trajectory_a = circle_trajectory(150, (175, 0), math.pi / 2, 60, 9 * math.pi / 2)
+                trajectory_b = circle_trajectory(150, (325, 0), math.pi / 2, 60, 9 * math.pi / 2)
+                trajectory_b.reverse()
 
-            trajectory_a_1 = circle_trajectory(125, (125, 0), math.pi / 2, 60, 7 * math.pi / 2)
-            trajectory_a_2 = circle_trajectory(125, (175, 0), 3 * math.pi / 2, 60, 11 * math.pi / 2)
-            trajectory_a_2.reverse()
+                trajectory_a_1 = circle_trajectory(125, (125, 0), math.pi / 2, 60, 7 * math.pi / 2)
+                trajectory_a_2 = circle_trajectory(125, (175, 0), 3 * math.pi / 2, 60, 11 * math.pi / 2)
+                trajectory_a_2.reverse()
 
-            trajectory_b_1 = circle_trajectory(125, (325, 0), math.pi / 2, 60, 7 * math.pi / 2)
-            trajectory_b_2 = circle_trajectory(125, (375, 0), 3 * math.pi / 2, 60, 7 * math.pi / 2)
-            trajectory_b_2.reverse()
+                trajectory_b_1 = circle_trajectory(125, (325, 0), math.pi / 2, 60, 7 * math.pi / 2)
+                trajectory_b_2 = circle_trajectory(125, (375, 0), 3 * math.pi / 2, 60, 7 * math.pi / 2)
+                trajectory_b_2.reverse()
 
-            trajectory_alpha = trajectory_1.copy()
-            trajectory_alpha.extend(trajectory_a)
-            trajectory_alpha.extend(trajectory_a_1)
+                trajectory_alpha = trajectory_1.copy()
+                trajectory_alpha.extend(trajectory_a)
+                trajectory_alpha.extend(trajectory_a_1)
 
-            trajectory_beta = trajectory_1.copy()
-            trajectory_beta.extend(trajectory_a)
-            trajectory_beta.extend(trajectory_a_2)
+                trajectory_beta = trajectory_1.copy()
+                trajectory_beta.extend(trajectory_a)
+                trajectory_beta.extend(trajectory_a_2)
 
-            trajectory_gamma = trajectory_1.copy()
-            trajectory_gamma.extend(trajectory_b)
-            trajectory_gamma.extend(trajectory_b_1)
+                trajectory_gamma = trajectory_1.copy()
+                trajectory_gamma.extend(trajectory_b)
+                trajectory_gamma.extend(trajectory_b_1)
 
-            trajectory_omega = trajectory_1.copy()
-            trajectory_omega.extend(trajectory_b)
-            trajectory_omega.extend(trajectory_b_2)
+                trajectory_omega = trajectory_1.copy()
+                trajectory_omega.extend(trajectory_b)
+                trajectory_omega.extend(trajectory_b_2)
 
-            # Ok now we create a WHOLE lot of bugs to travel 4 different paths
-            for i in range(200):
-                if i%4 == 0:
-                    swarm = create_swarmer(trajectory_alpha)
-                elif i%4 == 1:
-                    swarm = create_swarmer(trajectory_beta)
-                elif i%4 == 2:
-                    swarm = create_swarmer(trajectory_gamma)
-                elif i%4 == 3:
-                    swarm = create_swarmer(trajectory_omega)
+                # Ok now we create a WHOLE lot of bugs to travel 4 different paths
+                for i in range(200):
+                    if i%4 == 0:
+                        swarm = create_swarmer(trajectory_alpha)
+                    elif i%4 == 1:
+                        swarm = create_swarmer(trajectory_beta)
+                    elif i%4 == 2:
+                        swarm = create_swarmer(trajectory_gamma)
+                    elif i%4 == 3:
+                        swarm = create_swarmer(trajectory_omega)
 
-                swarm.center_x, swarm.center_y = SCREEN_WIDTH + 40 * (i + 1), SCREEN_HEIGHT
-                enemy_row.append(swarm)
+                    swarm.center_x, swarm.center_y = SCREEN_WIDTH + 40 * (i + 1), SCREEN_HEIGHT
+                    enemy_row.append(swarm)
 
-            self.enemy_list.append(enemy_row)
+                self.enemy_list.append(enemy_row)
+
+            if self.time > 55 and self.wave == 8:
+                self.wave += 1
+                """
+                BOSS TIME
+                """
+                boss_line = Horizontal_Battle_Line(200, 300, 1, num_ships=1, depth=SCREEN_HEIGHT-50)
+                big_goo = Bullet(filename="blue_laser.png", speed=2, scale=2.5)
+                big_weapon = Weapon(friendly=False, requires_ammo=False, bullet_type=big_goo, bullet_speed=2)
+                destination = [(SCREEN_WIDTH/2, SCREEN_HEIGHT + 100), (SCREEN_WIDTH/2, SCREEN_HEIGHT - 50)]
+                boss = Enemy("Player.png", destination_list=destination,
+                             scale=.2,
+                             hp=150,
+                             weapon=big_weapon,
+                             speed_to_formation=2,
+                             time_between_firing=1.5,
+                             speed_in_formation=1,
+                             time_until_charge=300
+                             )
+                boss.center_x, boss.center_y = SCREEN_WIDTH/2, SCREEN_HEIGHT + 200
+                boss.angle = 180
+                boss_line.add_enemy(0, boss)
+                self.enemy_list.append(boss_line)
+                print("Boss joined the party")
+
 
         # Check if enemies are ready to fire
         for enemy_row in self.enemy_list:
